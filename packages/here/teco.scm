2f6a5d310e96d1d95cb1dec07b09c0206bfd8db0
Gunter Liszewski (s70-11) <gunter.liszewski@gmail.com>
2018-09-04 16:58:47 +0100

    ci: for deployment testing, focused on guix packaging

--- packages/here/teco.scm
+++ packages/here/teco.scm
@@ -19,15 +19,15 @@
 (define-public teco
   (package 
    (name "teco")
-   (version "0.0.52-42dd")
+   (version "0.0.53-8116")
    (source (origin
 	    (method git-fetch)
 	    (uri (git-reference
 		  (url "https://github.com/glisze/teco.git")
-		  (commit "deploy/v0.0.52-42dd")))
+		  (commit "deploy/v0.0.53-8116")))
 	    (sha256
 	     (base32
-	    "13rc26wdv2x4kyz1b0mxkz0dsdihnhmn89cj90qz5z31gbs1npj2"))))
+	    "070js4vpcq9n39jb12dalj8sca3w206hasig705sp5dr16km5nv6"))))
    (build-system gnu-build-system)
    (arguments
     '(#:phases
@@ -37,18 +37,19 @@
 				(for-each (lambda (file)
 					    (patch-shebang file))
 					  (find-files "./scripts/"))
-				(invoke "sh" "-c" "echo 0.0.52-42dd > ./VERSION")
+				(invoke "sh" "-c" "echo 0.0.53-8116 > ./VERSION")
 				(invoke "sh" "-c" "touch ./ChangeLog")
 				(invoke "sh" "-c" "sed NEWS.in -e 1s,@VERSION[@],$( cat ./VERSION ), > NEWS")		
 				(invoke "sh" "-c" "autoreconf -vif"))))))
+   (inputs
+    '(("ncurses",ncurses)))
    (native-inputs
     `(("autoconf" ,autoconf)
       ("automake" ,automake)
       ("perl" ,perl)
       ("git" ,git)
       ("texinfo" ,texinfo)
-      ("pkg-config",pkg-config)
-      ("ncurses",ncurses)))
+      ("pkg-config",pkg-config)))
    (synopsis "teco: @SYNOPSIS@")
    (description
     "@DESCRIPTION@")
