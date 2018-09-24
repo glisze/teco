cf448200d4467c3ae366356be5b458c182e36e90
Gunter Liszewski (s70-11) <gunter.liszewski@gmail.com>
2018-09-24 10:41:49 +0100

    teco-c branch: address compiler warnings
    and review earlier 'sticky plasters' for compiler errors.
    
    starting with an ncurses example 'from the book' to facilitate
    configure issues. In particular a '-lncurses' '-lgpm' '-lncureses'
    repot, and suspected to affect 'Cosmic' (or so)
    
    also, aim to compile both with --with-curses (default) and
    --without-curses (which may be better if teco is used in similar
    ways than sed; in a very 'terse' (terse is not impolite in my book)

---
+++ testsrc/curses/8100/0.c
@@ -0,0 +1,55 @@
+/* 8100/0.c using curses */
+/* https://github.com/gittub/ncurses/doc/ncurses-intro.doc */
+#include <curses.h>
+#include <signal.h>
+
+extern void exit(int);
+static void z(int s);
+
+int
+main(int argc, char* argv[])
+{
+  int a = 0;
+
+  /* time to do our stuff, here */
+
+  (void) signal(SIGINT, z); /* prepare our exit */
+
+  (void) initscr();         /* CURSES things    */
+  keypad(stdscr, TRUE);     /* TRUE convenience */
+  (void) nonl();            /* not s,NL,CR NL,  */
+  (void) cbreak();          /* char direct      */
+  (void) echo();            /* let's see this   */
+  if (has_colors())
+  {
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(4, COLOR_BLUE,   COLOR_BLACK);
+    init_pair(5, COLOR_CYAN,   COLOR_BLACK);
+    init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(7, COLOR_WHITE,   COLOR_BLACK);
+   }
+
+   for (;;) 
+   {
+     int b = getch();
+     attrset(COLOR_PAIR(a % 8));
+     a++;
+
+     /* do our thing, given the key */
+   }
+
+   z(0);                     /* if ever here, exit */
+}
+
+static void z(int a)
+{
+  endwin();
+
+  /* our own cleaning, now */
+
+  exit(0);
+
+}
